66d3841bee7f665ccade4d32324f85581fae1136
Rajshree <rajshreebhuyan1306@gmail.com>
2026-07-28 10:01:18 +0530

    ex1.js updated

---
+++ ex1.js
@@ -0,0 +1,17 @@
+// ES-6
+// 1.let and const
+// 2.arrow function
+// 3.promise
+// 4.filter,map,reduce
+// 5.template literal
+// 6.default value
+// 7.destructing
+// 8.rest and spread operator
+// 9.fetch
+// 10.set and map
+// 11.class component
+var a=20;
+const b=30;
+b=a++;
+console.log("a=",a);
+console.log("b=",b);
